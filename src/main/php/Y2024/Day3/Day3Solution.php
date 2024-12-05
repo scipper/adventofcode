45bbcd4330059326ff160a9f47176fb0b295d9dd
Steffen Kowalski <scipper@myscipper.de>
2024-12-05 21:29:30 +0100

    refactoring

--- src/main/php/Y2024/Day3/Day3Solution.php
+++ src/main/php/Y2024/Day3/Day3Solution.php
@@ -5,21 +5,35 @@ namespace AdventOfCode\Y2024\Day3;
 use Throwable;
 
 class Day3Solution {
+
     public function multiply(string $input): int {
         $sum = 0;
-        preg_match_all("/mul\(\d{1,3},\d{1,3}\)/", $input, $matches);
-        foreach ($matches[0] as $mul) {
-            $numbers = str_replace(["mul(", ")"], "", $mul);
-            $digits = explode(",", $numbers);
-
-            try {
-                $result = $digits[0] * $digits[1];
-                $sum += $result;
-            } catch(Throwable $_) {
-            }
+        foreach ($this->getMuls($input) as $mul) {
+            $sum += $this->getResultOfMul($mul, $sum);
         }
 
         return $sum;
+    }
+
+    private function getMuls(string $input): array {
+        preg_match_all($this->detectMul(), $input, $matches);
+        return $matches[0];
+    }
+
+    private function detectMul(): string {
+        return "/mul\(\d{1,3},\d{1,3}\)/"; // matches "mul(1-3 digits,1-3 digits)"
+    }
+
+    private function getResultOfMul(string $mul): int {
+        $numbers = str_replace(["mul(", ")"], "", $mul);
+        $digits = explode(",", $numbers);
+
+        try {
+            return $digits[0] * $digits[1];
+        }
+        catch (Throwable $_) {
+            return 0;
+        }
     }
 
 }
